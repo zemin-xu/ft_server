32ad8b386cce12fb201c4c97a7a46d71aa4dff10
Zemin XU <zeminxu@Zemins-MacBook-Pro.local>
2020-07-03 23:24:45 +0200

    test for pma

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,7 @@
 
+sudo docker stop ft_server 
+sudo docker system prune
+sudo docker rmi $(docker images -a -q)
 
 sudo docker build -t ft_server .
 sudo docker run -it -p 80:80 -p 443:443 ft_server
