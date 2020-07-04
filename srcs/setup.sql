2f4a0e1376512fcc764c4fcf9493f180c4097990
Zemin XU <zeminxu@Zemins-MacBook-Pro.local>
2020-07-04 23:43:51 +0200

    add account

--- srcs/setup.sql
+++ srcs/setup.sql
@@ -6,4 +6,10 @@ GRANT ALL PRIVILEGES ON wordpressDB.* TO 'root'@'localhost';
 
 FLUSH PRIVILEGES;
 
+CREATE USER 'zemin'@'localhost' identified by '123456';
+
+GRANT ALL PRIVILEGES ON wordpressDB.* TO 'zemin'@'localhost';
+
+FLUSH PRIVILEGES;
+
 EXIT;
