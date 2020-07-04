e76587e86de4ce9817485cda1ca5733494553ce6
Zemin XU <zeminxu@Zemins-MacBook-Pro.local>
2020-07-05 00:10:50 +0200

    update password

--- srcs/setup.sql
+++ srcs/setup.sql
@@ -1,14 +1,6 @@
 CREATE DATABASE wordpressDB;
 
-CREATE USER 'root'@'localhost' IDENTIFIED BY 'password';
-
 GRANT ALL PRIVILEGES ON wordpressDB.* TO 'root'@'localhost';
-
-FLUSH PRIVILEGES;
-
-CREATE USER 'zemin'@'localhost' IDENTIFIED BY 'pass';
-
-GRANT ALL PRIVILEGES ON wordpressDB.* TO 'zemin'@'localhost';
 
 FLUSH PRIVILEGES;
 
